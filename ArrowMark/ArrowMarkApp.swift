f6ba29a4b63393b139ab2806bb3953931180e0fc
OfficeGaku <info@office-gaku.net>
2025-05-09 18:48:51 +0900

    Initial Commit

---
+++ ArrowMark/ArrowMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArrowMarkApp.swift
+//  ArrowMark
+//
+//  Created by オフィス岳 on 2025/05/09.
+//
+
+import SwiftUI
+
+@main
+struct ArrowMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
